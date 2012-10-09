03a66deb95870319b4179e2816acea08d6941b4c
Olivier Lagache <olagache@gmail.com>
2012-10-09 18:57:02 +0200

    Change application context to ROOT (path '/') #6

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -69,6 +69,9 @@ environments {
     }
 }
 
+// Change application context to ROOT
+grails.app.context = "/"
+
 // log4j configuration
 log4j = {
     // Example of changing the log pattern for the default console appender:
